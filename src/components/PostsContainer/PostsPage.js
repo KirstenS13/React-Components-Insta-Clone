6f5d3fa046bebbba9d71f021ea6561d4957cfabd
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-04-26 19:52:22 -0400

    mapped over dummyData in PostsPage.js to create a post for each set of data, got it to work

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -13,6 +13,10 @@ const PostsPage = () => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {dummyData.map((postData) => {
+        console.log(postData);
+        return <Post post={postData}/>
+      })}
     </div>
   );
 };
