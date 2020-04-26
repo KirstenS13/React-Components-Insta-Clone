dc00989b2744172cf2c7c80deaa49ffab9d26665
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-04-26 19:35:28 -0400

    imported dummy-data.js into PostsPage.js, set up state for the likes from the dummy data in PostsPage.js

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,9 +4,12 @@ import React, { useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from "../../dummy-data.js";
 
 const PostsPage = () => {
   // set up state for your data
+  const [likes, setLikes] = useState();
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
